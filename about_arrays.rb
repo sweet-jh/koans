7df149b7aadd30d42be3d3071eff09817298406c
hjing <hjing@thoughtworks.com>
2016-08-10 18:06:33 +0800

    test git2

--- about_arrays.rb
+++ about_arrays.rb
@@ -1,25 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-class AboutArrays < Neo::Koan  #?????????????
-  def test_creating_arrays
-    empty_array = Array.new
-    assert_equal Array, empty_array.class
-    assert_equal 0, empty_array.size
-  end
-
-  def test_array_literals
-    array = Array.new
-    assert_equal [], array
-
-    array[0] = 1
-    assert_equal [1], array
-
-    array[1] = 2
-    assert_equal [1, 2], array
-
-    array << 333
-    assert_equal [1,2,333], array
-  end
+# class AboutArrays < Neo::Koan  #?????????????
+#   def test_creating_arrays
+#     empty_array = Array.new
+#     assert_equal Array, empty_array.class
+#     assert_equal 0, empty_array.size
+#   end
+#
+#   def test_array_literals
+#     array = Array.new
+#     assert_equal [], array
+#
+#     array[0] = 1
+#     assert_equal [1], array
+#
+#     array[1] = 2
+#     assert_equal [1, 2], array
+#
+#     array << 333
+#     assert_equal [1,2,333], array
+#   end
 
   def test_accessing_array_elements
     array = [:peanut, :butter, :and, :jelly]
